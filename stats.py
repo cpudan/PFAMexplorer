65488ef43f07bc86932ed5d176fc40a5d8f343dd
Dan Meyer <cpudan@gmail.com>
2017-12-13 16:15:43 -0500

    stats to gather stats about families

---
+++ stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from schema2 import session, Family
+from multiprocessing import Pool
+import json
+
+
+def getFamStats(fam):
+    session.add(fam)
+    protnames = set([])
+    gounion = set([])
+    for a in fam.proteins:
+        pname = a.protein.name
+        protnames.add(pname)
+        gounion |= set([term.name for term in a.protein.GOterms])
+    famsize = len(protnames)
+    return (famsize, len(gounion))
+
+
+if __name__ == '__main__':
+    with Pool() as p:
+        allfams = session.query(Family).all()
+        multiple_results = \
+            [p.apply_async(getFamStats, [fam]) for fam in allfams]
+        numfams = len(multiple_results)
+        sizes, funcs = [], []
+        for res in multiple_results:
+            (famsize, numfuncs) = res.get()
+            sizes.append(famsize)
+            funcs.append(numfuncs)
+        output = {}
+        output['sizes'] = sizes
+        output['funcs'] = funcs
+        print(json.dumps(output, sort_keys=False, indent=2,
+              separators=(',', ': ')))
